e47b35f260dc92b3654f99d589700a06cbbc8703
Delfie Deril <delfiederil30@gmail.com>
2025-12-20 19:03:30 +0530

    Update index.php

--- Online_Train_Reservation_System/index.php
+++ Online_Train_Reservation_System/index.php
@@ -1,1 +1,27 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="card">
+    <h2>🚆 Online Train Reservation System</h2>
+<p class="subtitle">
+    Reserve, view, and manage your train tickets easily in one place.
+</p>
+    <form method="POST" action="login.php">
+        <input type="text" name="username" placeholder="Username" required>
+
+        <input type="password" name="password" placeholder="Password" required>
+
+        <button class="btn" type="submit">Login</button>
+    </form>
+    <a href="register.php" class="link">New user? Register here</a>
+</div>
+</body>
+</html>
 
